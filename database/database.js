f50eb7ac2f80f653100bb1527fa3adad57b5a47a
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-07-31 12:43:52 +0100

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,1 +1,28 @@
 
+const fs = require("fs");
+const path = require("path");
+
+function ler(nome) {
+    const arquivo = path.join(__dirname, `${nome}.json`);
+
+    if (!fs.existsSync(arquivo)) {
+        fs.writeFileSync(arquivo, "[]");
+    }
+
+    return JSON.parse(fs.readFileSync(arquivo, "utf8"));
+}
+
+function salvar(nome, dados) {
+    const arquivo = path.join(__dirname, `${nome}.json`);
+
+    fs.writeFileSync(
+        arquivo,
+        JSON.stringify(dados, null, 4),
+        "utf8"
+    );
+}
+
+module.exports = {
+    ler,
+    salvar
+};
