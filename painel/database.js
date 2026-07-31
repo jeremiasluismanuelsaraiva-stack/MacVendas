b513aa63e19d68ee4ecb45c642e67c7c3cfb4adb
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-07-31 09:13:17 +0100

    Add dashboard and vendas functions to database.js

--- painel/database.js
+++ painel/database.js
@@ -1,1 +1,30 @@
 
+function dashboard() {
+
+    return {
+
+        vendas: 0,
+        faturamento: 0,
+        clientes: 0,
+        dispositivos: 0,
+        lucro: 0,
+        pedidos: 0,
+        transferencias: 0,
+        stock: 0
+
+    };
+
+}
+
+function vendas(){
+
+    return [];
+
+}
+
+module.exports={
+
+dashboard,
+vendas
+
+};
