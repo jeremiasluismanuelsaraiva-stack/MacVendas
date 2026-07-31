a5bf48abe59f1cfefc72006a2d5288867ca2e8bd
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-07-31 13:01:23 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,25 +1,53 @@
-
-
 const express = require("express");
 const cors = require("cors");
-const db = require("./database");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.static("./"));
 
-app.get("/dashboard", (req, res) => {
-    res.json(db.dashboard());
+app.use(express.json({
+    limit: "50mb"
+}));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// ROTAS
+app.use("/dashboard", require("./dashboard"));
+app.use("/vendas", require("./vendas"));
+app.use("/clientes", require("./clientes"));
+app.use("/pedidos", require("./pedidos"));
+app.use("/dispositivos", require("./dispositivos"));
+
+// STATUS
+app.get("/", (req, res) => {
+
+    res.json({
+
+        success: true,
+
+        nome: "Sistema SSD API",
+
+        versao: "1.0.0",
+
+        status: "Online"
+
+    });
+
 });
 
-app.get("/vendas", (req, res) => {
-    res.json(db.vendas());
+// ROTA NÃO ENCONTRADA
+app.use((req, res) => {
+
+    res.status(404).json({
+
+        success: false,
+
+        error: "Rota não encontrada."
+
+    });
+
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log("Painel iniciado");
-});
+module.exports = app;
