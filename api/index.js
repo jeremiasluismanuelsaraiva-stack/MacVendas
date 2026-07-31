e3d1d28bf82845dbd9b13b3bfa59e77321c7aa24
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-07-31 09:30:23 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,25 @@
 
+
+const express = require("express");
+const cors = require("cors");
+const db = require("./database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./"));
+
+app.get("/dashboard", (req, res) => {
+    res.json(db.dashboard());
+});
+
+app.get("/vendas", (req, res) => {
+    res.json(db.vendas());
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Painel iniciado");
+});
