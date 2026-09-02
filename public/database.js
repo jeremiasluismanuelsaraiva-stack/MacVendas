ec1eb9a9b1c543e3cdda169efb3c964c9b181285
jeremiasluismanuelsaraiva-stack <jeremiasluismanuelsaraiva@gmail.com>
2026-09-02 23:34:44 +0200

    Update database.js

--- public/database.js
+++ public/database.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../database/database");
-
+const { db } = require("./firebase-admin");
 
 // =====================================================
 // DASHBOARD
